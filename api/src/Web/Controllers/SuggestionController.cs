34deb89db073a86c74d413d5c9807bfd63b9c05e
Marc-Antoine Dion <madion@spektrummedia.com>
2020-03-03 23:48:00 -0500

    Added base structure for the api. It is now possible to query data via the suggestion endpoint and get a JSON list of Geo values in return

---
+++ api/src/Web/Controllers/SuggestionController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using DefaultNamespace;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SuggestionApi.Web.Controllers
+{
+    [Route("suggestions/")]
+    [ApiController]
+    public class SuggestionController : ControllerBase
+    {
+        private readonly ILogger<SuggestionController> _logger;
+
+        public SuggestionController(ILogger<SuggestionController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]        
+        public List<SuggestionDto> GetSuggestions(string q)
+        {
+            return new List<SuggestionDto>
+            {
+                new SuggestionDto
+                {
+                    Name = "London, ON, Canada",
+                    Latitude = 42.98339,
+                    Longitude = -81.23304,
+                    Score = 0.9
+                }
+            };
+        }
+    }
+}
